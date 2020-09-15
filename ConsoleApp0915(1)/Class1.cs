6525b8e789b1fff2257c6289828b329cab21cc9d
이승원 <GDC6@DESKTOP-OE8BLQK>
2020-09-15 17:12:20 +0900

    테스트

---
+++ ConsoleApp0915(1)/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class MyCat
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Sum(double x, int y)
+        {
+            return (int)(x + y);
+        }
+    }
+
+    class Class1
+    {
+        static void Main()
+        {
+            MyCat c1 = new MyCat();
+
+            int result = MyCat.Sum(5, 3);
+            c1.Sum(2.5, 5);
+
+            Random rnd = new Random();
+            rnd.Next();
+
+            int[] arr = new int[3];
+            for(int i=0; i<arr.Length; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp0915(1)/Program_org.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0915_1_
+{
+    class Program_org
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
